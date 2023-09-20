32c41d81b3285ae8b507ab4bfdd84373f2170124
bob-skywalker <q463250938@gmail.com>
2023-09-19 19:11:56 -0700

    CoinRowView completed with Double extension for easy string conversion & number formatting

--- CryptoX/Core/Home/Views/CoinRowView.swift
+++ CryptoX/Core/Home/Views/CoinRowView.swift
@@ -13,6 +13,40 @@ struct CoinRowView: View {
     
     var body: some View {
         HStack(spacing: 0){
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                middleColumn
+            }
+            
+            Spacer()
+            
+            rightColumn
+        }
+        .font(.subheadline)
+        .padding(.horizontal, 5)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0){
+            
             Text("\(coin.rank)")
                 .font(.caption)
                 .foregroundColor(Color.secondaryTextColor)
@@ -25,13 +59,23 @@ struct CoinRowView: View {
                 .font(.headline)
                 .padding(.leading, 5)
                 .foregroundColor(Color.myAccentColor)
-            
-            Spacer()
-            if showHoldingsColumn {
-                VStack {
-                }
+        }
+    }
+    
+    private var middleColumn: some View{
+        HStack {
+            VStack(alignment: .trailing) {
+                Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                    .bold()
+                Text((coin.currentHoldings ?? 0).asNumberString())
             }
+            .foregroundColor(Color.myAccentColor)
             
+        }
+    }
+    
+    private var rightColumn: some View {
+        HStack {
             VStack(alignment: .trailing){
                 Text("\(coin.currentPrice.asCurrencyWith6Decimals())")
                     .bold()
@@ -39,12 +83,7 @@ struct CoinRowView: View {
                 Text("\(coin.priceChangePercentage24H?.asPercentString() ?? "0%")")
                     .foregroundColor(((coin.priceChangePercentage24H ?? 0) >= 0) ? Color.greenColor : Color.redColor)
             }
+            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
         }
-    }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
     }
 }
--- CryptoX/Extensions/Double.swift
+++ CryptoX/Extensions/Double.swift
@@ -12,6 +12,32 @@ extension Double {
     /// Converts a Double -> Currency with 2-6 decimal places
     /// ```
     /// Convert 1234.56 to $ 1,234.56
+    /// Convert 1.23456 to $ 1.23
+    /// ```
+    private var currentFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    /// Converts a Double -> Currency as a String with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $ "1,234.56"
+    /// Convert 1.23456 to $ "1.23456"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currentFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    
+    /// Converts a Double -> Currency with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $ 1,234.56
     /// Convert 1.23456 to $ 1.23456
     /// ```
     private var currentFormatter6: NumberFormatter {
